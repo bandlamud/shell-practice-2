1a557242a9006a6788ab002a4159599e993c82e6
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 13:02:38 +0530

    aading execution timing

--- 7-variable.sh
+++ 7-variable.sh
@@ -7,6 +7,6 @@ START_TIME=$(date +%s)
 sleep 10
 
 END_TIME=$(date +%s)
-TOTAL_TIME=$($END_TIME-$START_TIME)
+TOTAL_TIME=$(($END_TIME-$START_TIME))
 
 echo "script execution is $TOTAL_TIME in seconds"

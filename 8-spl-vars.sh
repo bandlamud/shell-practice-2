aae33978f37eb0245f30075478a977db2e13559f
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 19:58:25 +0530

    modifieng scripts name

--- 8-spl-vars.sh
+++ 8-spl-vars.sh
@@ -3,3 +3,6 @@
 echo "ALL variable passed to the scripts: $@"
 echo "ALL variable passed to the scripts: $*"
 echo "execution script : $0"
+echo "currect working directory is: $PWD"
+echo "who is running of the script is: $USER"
+echo "Home directory user is: $HOME"

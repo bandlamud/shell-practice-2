04e885d3b7a2584b9963bc7d15edf646a1a0796c
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 20:04:56 +0530

    providing pid

--- 8-spl-vars.sh
+++ 8-spl-vars.sh
@@ -7,3 +7,5 @@ echo "currect working directory is: $PWD"
 echo "who is running of the script is: $USER"
 echo "Home directory user is: $HOME"
 echo "PID of script is : $$"
+sleep 50 &
+echo "execution PID of last command is : $!"

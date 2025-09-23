53b72c1f7dfd885257932585755d25fbbbd531bb
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 11:30:01 +0530

    conversation

---
+++ 2-conversion.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Ramesh: Hi suresh how are you"
+echo "surech: Hi ramesh i am fine what about you"
+echo "ramesh: i am good what are you learning now"
+echo "suresh: i am learning shell scripting"

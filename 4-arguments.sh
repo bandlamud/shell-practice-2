c866425b96bfb765b07265b2193be66948fc5039
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 11:42:23 +0530

    giving arguments

---
+++ 4-arguments.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+
+echo "lakshmi: Hi Durga how are you"
+echo "Durga: i am good lakshmi where are you now"
+echo "lakshmi: i am in kommi village what about you"
+echo "Durga: i am in bangalore"
+echo "Bharath: i am also in bangalore for studies puropuse"

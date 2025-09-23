31260bb476f49f32f980c0378d78eed66daa09e3
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-23 11:33:50 +0530

    replacing the names

---
+++ 3-variable.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1= "Balu"
+PERSON2= "Bharath"
+
+echo "$PERSON1: Hi $PERSON2 how are you"
+echo "$PERSON2: Hi $PERSON1 i am fine what about you"
+echo "$PERSON1: i am good what are you learning now"
+echo "$PERSON2: i am learning shell scripting"

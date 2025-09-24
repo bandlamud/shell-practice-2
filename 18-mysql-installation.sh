d5ad3138291acd4eedca42c5888dd5db390de131
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-24 22:44:12 +0530

    installing mysql with root access

--- 18-mysql-installation.sh
+++ 18-mysql-installation.sh
@@ -2,14 +2,16 @@
 
 USERID=$(id -u)
 
-if [ $USERID -ne 0]; then
+if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run this script with root previlegies"
+    exit 1 # other then zero we can give number
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: installing mysql is failure"
+    exit 1
 else
     echo "installing mysql is success"
 fi

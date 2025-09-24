df7893126c6c584fe524b56bb18f1577f8666163
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-24 06:59:40 +0530

    aading spl

--- 14-spl-vars.sh
+++ 14-spl-vars.sh
@@ -3,3 +3,6 @@
 echo "All variable passed to the script is $@"
 echo "All variable passed to the script is $*"
 echo "execution script name is $0"
+echo "Present working directory is $PWD"
+echo "working Home directory is $HOME"
+echo "using home user is $USER"

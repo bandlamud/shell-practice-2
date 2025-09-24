ae8b4cbe94931331f82d750481c1c6f05b16d286
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-24 06:33:53 +0530

    arguments

---
+++ 12-arguments.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+PERSON3=$3
+PERSON4=$4
+
+echo "Subbu:: hey Balu where are you now"
+echo "Balu: i am in ap what about you"
+echo "Subbu: i am also ap in ongole"
+echo "Balu: what is your merrage date"
+echo "Sivakumar: Hi all"
+echo "Sandeep: Hello all"

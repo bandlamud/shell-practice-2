af6e1f486112ec283754f5d88389c4475ea0a7c0
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-24 06:22:38 +0530

    healthy conversation

---
+++ 11-conversation.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1=
+PERSON2=
+
+echo "Ramesh:: Hi surech how are you"
+echo "Suresh:: Hi Ramesh and good what abo you"
+echo "Ramesh:: i am also good suresh waht are you learning now"
+echo "Suresh:: i am learning shell scripting"

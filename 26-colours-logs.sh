df646e95775d3642ac0b1420b91c811c5a3fae84
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-25 19:50:46 +0530

    adding colours

---
+++ 26-colours-logs.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root privilages"
+    exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0 ]; then
+        echo "ERROR:: installing $2 is failure"
+        exit 1
+    else
+        echo "instaling $2 is success"
+    fi
+}
+
+dnf list installed mysql
+# install it is not found
+if [ $? -ne 0 ]; then
+    dnf install mysql -y
+    VALIDATE $? "mysql"
+else
+    echo "mysql already installed.. $Y SKIPPING $N"
+fi
+
+dnf list installed nginx
+# install is not found
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    VALIDATE $? "NGINX"
+else
+    echo "nginx already installed.. $Y SKIPPING $N"
+fi
+
+dnf installed list python3
+# install is not found
+if [ $? -ne 0 ]; then
+    dnf install python3 -y
+    VALIDATE $? "PYTHON3"
+else
+    echo "python3 already installed.. $Y SKIPPING $n"
+fi

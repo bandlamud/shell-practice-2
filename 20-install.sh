589e45a95d0ef56e87c46678171ee3f8bf6df70c
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-25 10:28:06 +0530

    installing pckeges

---
+++ 20-install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+USERID=$(id -u)
+
+if [ $USERID -ne 0 ]; then
+    echo "ERROR:: Please run the script with root Privilages"
+fi
+
+dnf install mysql -y
+
+if [ $? -ne 0 ]; then
+    echo "ERROR:: installing mysql failure"
+else
+    echo "installing mysql is success"
+fi

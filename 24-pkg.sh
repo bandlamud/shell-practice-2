3cc647cc5580b8da9b327ff92361838329fe0945
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-25 19:16:24 +0530

    validation if root privilages

--- 24-pkg.sh
+++ 24-pkg.sh
@@ -4,11 +4,13 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root privilages"
+    exit 1
 fi
 
 dnf install mysql -y
 if [ $? -ne 0 ]; then
     echo "ERROR:: installing mysql is failure"
+    exit 1
 else
     echo "instaling mysql is success"
 fi
@@ -16,6 +18,7 @@ fi
 dnf install nginx -y
 if [ $? -ne 0 ]
     echo "ERROR:: installing nginx is failure"
+    exit 1
 else
     echo "installing nginx is success"
 fi
@@ -23,5 +26,6 @@ fi
 dnf install python3 -y
 if [ $? -ne 0 ]; then
     echo "ERROR:: instaling python3 is failure"
+    exit 1
 else
     echo "ERROR:: installing python3 is success"

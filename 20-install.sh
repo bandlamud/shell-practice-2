15f96af7b47c080d72adabec7a4f8bdddf6e97d9
bharathchandu302 <bharathchandu302@gmail.com>
2025-09-25 10:29:27 +0530

    installing pckeges

--- 20-install.sh
+++ 20-install.sh
@@ -4,12 +4,14 @@ USERID=$(id -u)
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR:: Please run the script with root Privilages"
+    exit 1
 fi
 
 dnf install mysql -y
 
 if [ $? -ne 0 ]; then
     echo "ERROR:: installing mysql failure"
+    exit 1
 else
     echo "installing mysql is success"
 fi
